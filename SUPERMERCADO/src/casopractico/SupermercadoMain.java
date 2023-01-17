286cdf2cb57d46c74e9737f42b7a92fe208987b5
Alvaro Barcena <alvarobarcena27@gmail.com>
2023-01-17 12:06:34 +0100

    Syso de bienvenida

--- SUPERMERCADO/src/casopractico/SupermercadoMain.java
+++ SUPERMERCADO/src/casopractico/SupermercadoMain.java
@@ -3,7 +3,8 @@ package casopractico;
 public class SupermercadoMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		System.out.println("Bienvenido al supermercado");
 
 	}
 
